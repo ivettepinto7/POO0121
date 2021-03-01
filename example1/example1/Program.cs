148a4e058dcb587cc6da856054d4fa74713e1c60
MiGIGiM <00211919@uca.edu.sv>
2021-03-01 11:54:46 -0600

    Adding fix sentence

--- example1/example1/Program.cs
+++ example1/example1/Program.cs
@@ -6,14 +6,16 @@ namespace example1
     {
         public static void Main(string[] args)
         {
-            int a = 0, b = 0, c = 0;
+            int a = 0, b = 0;
             Console.WriteLine("Enter a value:");
             a = Int32.Parse(Console.ReadLine() );
             Console.WriteLine("Enter another value:");
             b = Int32.Parse(Console.ReadLine());
             Console.WriteLine($"Adding the values of {a} and {b} give: "+ sum(a,b));
-            c = sum(a, b) + 2;
-            Console.WriteLine($"Adding the values of {a} and {c} give: " + sum(a, c));
+            if (sum(a, b) % 2 == 0)
+                Console.WriteLine($"Adding {a} and {b} gives a even number");
+            else
+                Console.WriteLine($"Adding {a} and {b} gives a odd number");
 
         }
         public static int sum(int a, int b){
