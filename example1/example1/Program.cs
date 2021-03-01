bf0ee8522d20b2328c631d75663f3cc7e4fcd639
MiGIGiM <00211919@uca.edu.sv>
2021-02-28 23:24:05 -0600

    Add code

---
+++ example1/example1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace example1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+            Console.WriteLine("Enter a value:");
+            a = Int32.Parse(Console.ReadLine() );
+            Console.WriteLine("Enter another value:");
+            b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Adding the values of {a} and {b} give: "+ sum(a,b));
+            c = sum(a, b) + 1;
+            Console.WriteLine($"Adding the values of {a} and {c} give: " + sum(a, c));
+
+        }
+        public static int sum(int a, int b){
+            int suma = a + b;
+            return suma;
+        }
+    }
+}
