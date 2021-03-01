5f200cb805db39a6c06006d77150e63dc64bb5cf
MiGIGiM <00211919@uca.edu.sv>
2021-03-01 12:02:50 -0600

    Made changes

--- example1/example1/Program.cs
+++ example1/example1/Program.cs
@@ -12,10 +12,8 @@ namespace example1
             Console.WriteLine("Enter another value:");
             b = Int32.Parse(Console.ReadLine());
             Console.WriteLine($"Adding the values of {a} and {b} give: "+ sum(a,b));
-            if (sum(a, b) % 2 == 0)
-                Console.WriteLine($"Adding {a} and {b} gives a even number");
-            else
-                Console.WriteLine($"Adding {a} and {b} gives a odd number");
+            int c = sum(a, b) + 1;
+            Console.WriteLine(c);
 
         }
         public static int sum(int a, int b){
