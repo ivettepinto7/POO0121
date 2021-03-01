762b684eee769346f5c1d19e2c6e8eff59e642cf
MiGIGiM <00211919@uca.edu.sv>
2021-02-28 23:20:59 -0600

    Modify values

--- example1/example1/Program.cs
+++ example1/example1/Program.cs
@@ -6,12 +6,13 @@ namespace example1
     {
         public static void Main(string[] args)
         {
-            int a = 0, b = 0;
+            int a = 0, b = 0, c = 0;
             Console.WriteLine("Enter a value:");
             a = Int32.Parse(Console.ReadLine() );
             Console.WriteLine("Enter another value:");
             b = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("The result is : "+ sum(a,b));
+            Console.WriteLine($"Adding the values of {a} and {b} give: "+ sum(a,b));
+            
         }
         public static int sum(int a, int b){
             int suma = a + b;
