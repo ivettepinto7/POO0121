6e0dea50dbbdce735fff1f496c6a69e1d9c7f327
MiGIGiM <00211919@uca.edu.sv>
2021-03-01 18:23:28 -0600

    Aplying good practices

--- example1/example1/Program.cs
+++ example1/example1/Program.cs
@@ -7,23 +7,19 @@ namespace example1
         public static void Main(string[] args)
         {
             int a = 0, b = 0, c = 0;
+            
             Console.WriteLine("Enter a value:");
             a = Int32.Parse(Console.ReadLine() );
+            
             Console.WriteLine("Enter another value:");
             b = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"Adding the values of {a} and {b} give: "+ sum(a,b));
-            c = sum(a, b) + 2;
-            if (sum(a, b) % 2 == 0)
-                Console.WriteLine("The result is an even number");
-            else
-                Console.WriteLine("The result is an odd number");
-            int c = sum(a, b) + 1;
-            Console.WriteLine(c + "is the result");
+            
+            Console.WriteLine($"Adding the values of {a} and {b} give: {sum(a,b)}");
+            
 
         }
         public static int sum(int a, int b){
-            int suma = a + b;
-            return suma;
+            return a + b;
         }
     }
 }
